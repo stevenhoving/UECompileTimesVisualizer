7960ec3dd0f8c14f9d7b86e559529a540c48fa0c
Steven Hoving <stevenhoving@gmail.com>
2019-02-22 16:51:17 +0100

    force skip invalid filenames instead of reporting them.

--- main.py
+++ main.py
@@ -267,13 +267,14 @@ with open(log_file, "r", encoding="utf-8-sig") as file:
                 print("Skipping line " + line)
                 continue
 
-        extension = ""
+        extension = None
         for ext in [".h", ".cpp", ".rc", ".lib", ".dll", ".exe"]:
             if ext in line:
                 extension = ext
                 break
-        else:
-            assert False, "Invalid file name: " + line
+
+        if extension is None:
+            continue
 
         if configuration == "Unreal":
             line = line.split("]")[1]
@@ -281,7 +282,7 @@ with open(log_file, "r", encoding="utf-8-sig") as file:
         current_file = line.strip().split(extension)[0] + extension
         print(current_file)
 
-        if extension in [".rc", ".lib", ".dll", ".exe"]:
+        if extension in [".rc", ".lib", ".dll", ".exe", ".txt", ".stamp", ".obj"]:
             print("Skipping extension " + extension)
             continue
 
